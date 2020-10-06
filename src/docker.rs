6b3c98700ce5968205d37846a0c688e53ce7f8a1
Nicolas De Loof <nicolas.deloof@gmail.com>
2020-10-06 18:22:54 +0200

    collect HTTP headers 🚧
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

---
+++ src/docker.rs
@@ -0,0 +1,4 @@
+
+pub struct Docker {
+    socket: String
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use std::os::unix::net::UnixStream;
 use std::io;
 use std::io::prelude::*;
+use http::{HeaderMap, HeaderValue};
+use http::header::HeaderName;
+use std::str::FromStr;
 
-fn main() {
+fn main() {   
     match ping() {
         Ok(s) => println!("> {}", s),
         Err(e) => println!("something went wrong: {:?}", e),
     }
 }
 
-fn ping() -> io::Result<String> {
+fn ping() -> io::Result<String> {  
     let mut socket = UnixStream::connect("/var/run/docker.sock")?;
     socket.write_all(b"GET /_ping HTTP/1.1\r\nHost: docker\r\nConnection: close\r\n\r\n")?;
     
@@ -17,12 +20,12 @@ fn ping() -> io::Result<String> {
     let code = parse_http_status_code(&mut reader)?;
     println!("HTTP status code = {}", code);
 
+    parse_http_headers(&mut reader);
     let mut response = String::new();
-    reader.read_to_string(&mut response)?;
     return Ok(response);
 }
 
-fn parse_http_status_code(reader: &mut dyn io::BufRead) -> io::Result<String> {
+fn parse_http_status_code(reader: &mut impl io::BufRead) -> io::Result<String> {
     let mut line = String::new();
     reader.read_line(&mut line)?;
 
@@ -32,3 +35,19 @@ fn parse_http_status_code(reader: &mut dyn io::BufRead) -> io::Result<String> {
     return Ok(code)
 }
 
+fn parse_http_headers(reader: &mut impl io::BufRead) -> io::Result<HeaderMap> {
+    let mut headers = HeaderMap::new();
+    loop {
+        let mut line = String::new();
+        reader.read_line(&mut line)?;
+        line = line.trim().to_string();
+        if line.len() == 0 {
+            return Ok(headers)
+        }
+        let parts: Vec<&str> = line.split(':').collect();
+        println!(" {} = {}", parts[0], parts[1]);
+        let name = HeaderName::from_str(parts[0]).unwrap();
+        let value = HeaderValue::from_str(parts[1]).unwrap();
+        headers.append(name, value);
+    }
+}
